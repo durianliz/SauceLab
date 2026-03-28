469ebaf21cd79735901f566c64b373c8482f4096
durianliz <lizzie.tepes@gmail.com>
2026-03-28 20:42:55 +0000

    LogIn tests refactor

--- tests/LogIn/logIn.spec.ts
+++ tests/LogIn/logIn.spec.ts
@@ -1,13 +1,16 @@
-import { test } from '../../fixture/fixture';
-import { expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/loginPage';
 import { HomePage } from '../../pages/homePage';
 import { userCredentials } from '../../data/userCredentials';
 
 
-test('Log in with valid userData', async ({ page, logIn }) => {
+test('Log in with valid userData', async ({ page }) => {
 
     const homePage = new HomePage(page);
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigateToLoginPage();
+    await loginPage.performLogin(userCredentials.standardUser.email, userCredentials.standardUser.password);
 
     await expect(page.locator('.title')).toBeVisible();
     await expect(page).toHaveURL(homePage.homePageURL);
@@ -16,26 +19,29 @@ test('Log in with valid userData', async ({ page, logIn }) => {
 
 
 
-test('Log in with invalid userData', async ({ page }) => {
-  
-const loginPage = new LoginPage(page)
-const invalidData = [{...userCredentials.lockedOutUser, type: 'locked out user'}, {...userCredentials.invalidUser, type: 'invalid user'}, 
-    {...userCredentials.invalidEmail, type: 'invalid email'}, {...userCredentials.invalidPassword, type: 'invalid password'}];
+const invalidData = [{ ...userCredentials.lockedOutUser, type: 'locked out user' }, { ...userCredentials.invalidUser, type: 'invalid user' },
+{ ...userCredentials.invalidEmail, type: 'invalid email' }, { ...userCredentials.invalidPassword, type: 'invalid password' }];
 
-    for (let data of invalidData) {
+invalidData.forEach((data) => {
+    test(`Log in with invalid ${data.type}`, async ({ page }) => {
+
+        const loginPage = new LoginPage(page)
+
         await loginPage.navigateToLoginPage();
         await loginPage.emailInput.fill(data.email);
         await loginPage.passwordInput.fill(data.password);
-         await loginPage.loginButton.click();
+        await loginPage.loginButton.click();
 
-         await expect(loginPage.error, `Expected error message for ${data.type}`).toBeVisible();
-         await expect(page, `Expected URL for ${data.type}`).toHaveURL(loginPage.logInPageURL);
-         
-    }
-});
+        await expect(loginPage.error, `Expected error message for ${data.type}`).toBeVisible();
+        await expect(page, `Expected URL for ${data.type}`).toHaveURL(loginPage.logInPageURL);
 
-    
-    
-   
 
-    
+    });
+})
+
+
+
+
+
+
+
